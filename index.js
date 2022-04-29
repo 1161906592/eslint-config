bac226e47a9583030c2694fab0c9733760be97aa
liuyang.0826 <1161906592@qq.com>
2022-04-29 12:04:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
       tsx: true,
@@ -11,78 +11,100 @@ module.exports = {
     browser: true,
     node: true,
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', process.cwd() + '/src']],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+    },
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["import", "react"],
+  plugins: ['import', 'react'],
   rules: {
     // eslint
-    eqeqeq: ["error", "always"],
-    "prefer-template": "error",
-    "dot-notation": "error",
-    "no-plusplus": "error",
+    eqeqeq: ['error', 'always'],
+    'prefer-template': 'error',
+    'dot-notation': 'error',
+    'no-plusplus': 'error',
 
     // prettier
-    "prettier/prettier": "warn",
+    'prettier/prettier': 'warn',
 
     // jsx
-    "react/jsx-curly-brace-presence": "error",
-    "react/jsx-no-undef": "error",
-    "react/jsx-no-duplicate-props": "error",
-    "react/jsx-uses-vars": "error",
-    "react/no-string-refs": "error",
-    "react/jsx-key": "error",
-    "react/jsx-no-comment-textnodes": "error",
-    "react/jsx-no-target-blank": "error",
-    "react/jsx-boolean-value": "error",
-    "react/no-unescaped-entities": "error",
-    "react/no-array-index-key": "error",
-    "react/self-closing-comp": "error",
+    'react/jsx-curly-brace-presence': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-string-refs': 'error',
+    'react/jsx-key': 'error',
+    'react/jsx-no-comment-textnodes': 'error',
+    'react/jsx-no-target-blank': 'error',
+    'react/jsx-boolean-value': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-array-index-key': 'error',
+    'react/self-closing-comp': 'error',
 
     // import
-    "import/first": "error",
-    "import/no-duplicates": "error",
-    "import/order": [
-      "error",
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
       {
-        groups: ["builtin", "external", "internal", "parent", "index", "object", "type", "sibling"],
+        groups: ['builtin', 'external', 'internal', 'parent', 'index', 'object', 'type', 'sibling'],
         pathGroups: [
           {
-            pattern: "vue",
-            group: "external",
-            position: "before",
+            pattern: 'vue',
+            group: 'external',
+            position: 'before',
           },
           {
-            pattern: "vue-*",
-            group: "external",
-            position: "before",
+            pattern: 'vue-*',
+            group: 'external',
+            position: 'before',
           },
           {
-            pattern: "@vue/**",
-            group: "external",
-            position: "before",
+            pattern: '@vue/**',
+            group: 'external',
+            position: 'before',
           },
           {
-            pattern: "@/**",
-            group: "internal",
-            position: "before",
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
           }
         ],
-        pathGroupsExcludedImportTypes: ["type"],
+        pathGroupsExcludedImportTypes: ['type'],
+        'newlines-between': 'never',
         alphabetize: {
           order: 'asc',
           caseInsensitive: false
         },
       },
     ],
+
+    // impprt-alias
+    'import-alias/import-alias': [
+      'error',
+      {
+        relativeDepth: 0,
+        aliases: [
+          { alias: '@', matcher: '^src' },
+        ],
+      },
+    ],
   },
   overrides: [
     {
-      files: "*.vue",
-      extends: ["plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+      files: '*.vue',
+      extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
     },
   ],
 };
